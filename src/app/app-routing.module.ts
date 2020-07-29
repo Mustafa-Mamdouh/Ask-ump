779fd990606f43edb01023455c74b57b4f0cba86
test <mustafa-mamdouh@outlook.com>
2020-07-29 19:52:54 +0200

    remove login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,9 @@ import { UmpTicketComponent } from './ump-ticket/ump-ticket.component'
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [{
-  path: 'main',
+  path: '',
   component: UmpTicketComponent,
   data: { appId: 0, title: 'Ask UMP' }
-},{
-  path: '',
-  component: LoginComponent,
-  data: { appId: 0, title: 'Login' }
 },{
   path: '**',
   redirectTo: 'main',
